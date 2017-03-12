5983c5fdae94541f7e80d2e99c4a35dbe17b6afb
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-03-12 16:51:35 -0500

    Adds links to detail page

--- app/snippets/snippetapp/snippets/urls.py
+++ app/snippets/snippetapp/snippets/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from snippets import views
 
 urlpatterns = [
-    
+
     url(r'^home/$', views.home, name='home'),
-    url(r'^detail/(?P<snippet_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^home/detail/(?P<snippet_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^add/$', views.add, name="add")
 ]
