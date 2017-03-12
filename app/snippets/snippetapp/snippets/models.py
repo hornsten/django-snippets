f3eb49f3ae904f926c57768556a5daa5ef16a96f
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-03-12 13:42:18 -0500

    Able to add snippets to database and display detail

--- app/snippets/snippetapp/snippets/models.py
+++ app/snippets/snippetapp/snippets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Snippet(models.Model):
+    title = models.CharField(max_length=256, blank=False, null=False)
+    language = models.CharField(max_length=256, blank=False, null=False)
+    description = models.CharField(max_length=256, blank=False, null=False)
+    snippet = models.CharField(max_length=256, blank=False, null=False)
+
+    def __str__(self):
+        """ returns a human readable version of the student object """
+        return 'Snippet Title: {0},\n Language {1} \n Description: {2}\n Snippet: {3}\n'.format(self.title, self.language, self.description, self.snippet)
--- app/snippets/snippetapp/snippets/views.py
+++ app/snippets/snippetapp/snippets/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+from .models import Snippet
 # Create your views here.
-class HomePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+def add(request):
+    if request.method == 'POST':
+        # save dat data
+        Snippet(
+            title=request.POST['title'],
+            language=request.POST['language'],
+            description=request.POST['description'],
+            snippet=request.POST['snippet']
+        ).save()
+
+    return render(request, 'snippets/add.html', {})
+
+def home(request):
+    snippets = Snippet.objects.all()
+    context = {'snippets': snippets}
+    return render(request, 'snippets/home.html', context)
+
+def detail(request, snippet_id):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+        context = { 'snippet': snippet  }
+    except Snippet.DoesNotExist:
+        raise Http404('Aint no snippet by that id!')
+
+    return render(request, 'snippets/detail.html', context)
